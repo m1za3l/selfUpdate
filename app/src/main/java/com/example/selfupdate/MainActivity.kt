46114752f67feee9117911aad4561c2bbc8a4a6a
m1za3l <antoniopelaezmisael@gmail.com>
2023-07-07 12:32:16 -0600

    [add]test de commit, al hacer pull

--- app/src/main/java/com/example/selfupdate/MainActivity.kt
+++ app/src/main/java/com/example/selfupdate/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //test de commit, al hacer pull
+        //test de commit, al hacer push
     }
 }
